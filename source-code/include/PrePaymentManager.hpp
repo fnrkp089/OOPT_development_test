3da62bc3e2cfdcccce3b3f883e9076805d07bcb5
J3rry33 <lancia01@naver.com>
2025-05-17 15:27:51 +0900

    5/17 추가구현

--- source-code/include/PrePaymentManager.hpp
+++ source-code/include/PrePaymentManager.hpp
@@ -1,21 +1,22 @@
-#ifndef PREPAYMENTMANAGEMENT_H
-#define PREPAYMENTMANAGEMENT_H
+#ifndef PrePaymentManager_H
+#define PrePaymentManager_H
 
 #include "Bank.hpp"
+#include "AuthCodeManager.hpp"
 #include <string>
 
 using namespace std;
 
-class PrePaymentManagement {
+class PrePaymentManager {
 private:
 
 public:
     // 생성자
-    PaymentManagement();
+    PrePaymentManager();
 
     // 메서드
-    void requestPrePayment(std::string dvmId, AuthCode authCode); // 선결제 요청
-    void acceptPrePayment(AuthCode authCode); // 선결제 수락
+    void requestPrePayment(std::string dvmId, const AuthCode& authCode); // 선결제 요청
+    void acceptPrePayment(const AuthCode& authCode); // 선결제 수락
 };
 
-#endif // PREPAYMENTMANAGEMENT_H
+#endif // PrePaymentManager_H
--- source-code/src/PrePaymentManager.cpp
+++ source-code/src/PrePaymentManager.cpp
@@ -1,18 +1,18 @@
 #include "PrePaymentManager.hpp"
 
 // 생성자
-PrePaymentManagement::PrePaymentManagement() {
+PrePaymentManager::PrePaymentManager() {
     // 생성자 구현
 }
 
 // 선결제 요청
-void PrePaymentManagement::requestPrePayment(std::string dvmId, AuthCode authCode) {
+void PrePaymentManager::requestPrePayment(std::string dvmId, const AuthCode& authCode) {
     // 선결제 요청 구현
     // dvmId와 authCode를 사용하여 선결제 요청을 처리합니다.
 }
 
 // 선결제 수락
-void PrePaymentManagement::acceptPrePayment(AuthCode authCode) {
+void PrePaymentManager::acceptPrePayment(const AuthCode& authCode) {
     // 선결제 수락 구현
     // authCode를 사용하여 선결제를 수락합니다.
 }   
